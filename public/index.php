59ed16f108739b599cf3bb84a67d2592af60d6c7
PouyaniArmin <pouyaniarmin@gmail.com>
2025-12-28 11:08:29 +0330

    feat: bootstrap application in index.php with routes setup

--- public/index.php
+++ public/index.php
@@ -1,7 +1,19 @@
 <?php
 
 use App\Application;
+use App\Request;
+use App\Router;
+use Controllres\HomeController;
+use Controllres\HomeCotnteoller;
 
 require_once __DIR__."/../vendor/autoload.php";
 
-new Application;
+$app=new Application(new Router(new Request));
+$app->router->get('/',[HomeController::class,'index']);
+$app->router->get('/post/{id}',[HomeController::class,'test']);
+$app->router->get('/about/{id}',[HomeController::class,'new']);
+$app->router->get('/home',['test']);
+$app->router->get('/test',function(){
+    return "Test";
+});
+$app->run();
