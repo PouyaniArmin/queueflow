8578703a07ecf2a3845e32169026ae49567cbe31
PouyaniArmin <pouyaniarmin@gmail.com>
2025-12-28 11:07:29 +0330

    feat: add base controllers with HomeController example

---
+++ controllers/HomeController.php
@@ -0,0 +1,18 @@
+<?php
+namespace Controllres;
+
+use App\Controller;
+use App\Request;
+use App\View;
+
+class HomeController extends Controller{
+    public function index(Request $request){
+        return $this->view('home');
+    }
+    public function test($id){
+        return $id;
+    }
+    public function new(Request $request,$id){
+        return "id: $id";
+    }
+}
