a2f1a2450d999079e198aa5df7491d336ed408bf
PouyaniArmin <pouyaniarmin@gmail.com>
2025-12-30 09:34:26 +0330

    feat: handle RouteNotFoundException in Application with proper 404 response

--- app/Application.php
+++ app/Application.php
@@ -1,12 +1,28 @@
 <?php
+
 namespace App;
-class Application{
-    public Router $router;
-    public function __construct(Router $router)
-    {
-        $this->router=$router;
-    }
-    public function run(){
+
+use Exception;
+use Exceptions\RouteNotFoundException;
+
+class Application
+{
+  public Router $router;
+  public function __construct(Router $router)
+  {
+    $this->router = $router;
+  }
+  public function run()
+  {
+    try {
       echo $this->router->resolve();
+    } catch (RouteNotFoundException $e) {
+      http_response_code(404);
+      $view = new View;
+      echo $view->make(['view' => '404', 'data' => ['title' => 'page not Found'], 'layout' => 'main']);
+    } catch (Exception $e) {
+      http_response_code(500);
+      echo "Error Server 500 " . $e->getMessage();
     }
+  }
 }
