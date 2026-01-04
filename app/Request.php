83183fffcda3f0226cb6be8c4e8007e3db3f66ca
PouyaniArmin <pouyaniarmin@gmail.com>
2026-01-04 15:19:03 +0330

    feat(router): add HTTP version and query string accessors to Request Introduce getHttpVersion() and getQueryString() methods.

--- app/Request.php
+++ app/Request.php
@@ -1,16 +1,29 @@
-<?php 
+<?php
+
 namespace App;
-class Request{
-    
-    public function url(){
-        $path=$_SERVER['REQUEST_URI'];
-        $possition=strpos($path,"?");
-        if (!$possition) {
-           return $path;
+
+class Request
+{
+
+    public function url(): string
+    {
+        $path = $_SERVER['REQUEST_URI'];
+        if (($pos = strpos($path, '?')) !== false) {
+            $path = substr($path, 0, $pos);
         }
-        return substr($path,0,$possition);
+        return $path === '/' ? $path : rtrim($path, '/');
     }
-    public function method():string{
+    public function method(): string
+    {
         return strtolower($_SERVER['REQUEST_METHOD']);
+    }
+
+    public function getHttpVersion(): string
+    {
+        return $_SERVER['SERVER_PROTOCOL'];
+    }   
+    public function getQueryString()
+    {
+        return $_SERVER['QUERY_STRING'];
     }
 }
