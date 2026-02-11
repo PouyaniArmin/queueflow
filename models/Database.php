a284f7b3462c2cd2a520a9f93ed2f5ce19a4e6d7
PouyaniArmin <pouyaniarmin@gmail.com>
2026-02-11 09:56:46 +0330

    WIP: Database class singleton progress

--- models/Database.php
+++ models/Database.php
@@ -9,15 +9,16 @@ use PDOException;
 
 class Database
 {
+    private static $instance;
     private ?PDO $conn;
-    public function __construct()
+    private function __construct()
     {
-        try {
-            $dsn = "pgsql:host=172.17.0.2;port=5432;dbname=queueflow_db";
-            $this->conn = new PDO($dsn, 'armin', '1234');
-            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        } catch (PDOException $e) {
-            throw new Exception("Error Connection to postgres : {$e->getMessage()}");
-        }
+    }
+    private function __clone()
+    {
+        throw new \Exception('Not implemented');
+    }
+    private function __wakeup(){
+        
     }
 }
