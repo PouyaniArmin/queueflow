b6d62ab6ce2864e0d2fe460f793af007c8a061ed
PouyaniArmin <pouyaniarmin@gmail.com>
2026-02-26 15:17:07 +0330

    feat(views): add initial login and register view templates

---
+++ views/register.php
@@ -0,0 +1,17 @@
+<div class="container">
+    <h1>Register</h1>
+</div>
+<div class="container px-4 text-center">
+    <div class="row gx-6">
+        <div class="col-8">
+            <div class="p-3">Business</div>
+        </div>
+        <div class="col-4">
+            <div class="p-3">Customer</div>
+        </div>
+    </div>
+</div>
+
+<div class="container py-4">
+    <a href="/login">Login</a>
+</div>
