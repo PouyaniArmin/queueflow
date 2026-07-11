b6963a6da0363530992c75c4f91b130e379e1364
PouyaniArmin <pouyaniarmin@gmail.com>
2026-07-11 22:11:49 +0330

    feat: add dashboard controller

--- controllers/DashboardController.php
+++ controllers/DashboardController.php
@@ -2,10 +2,11 @@
 namespace Controllers;
 
 use App\Controller;
+use App\Request;
 
 class DashboardController extends Controller
 {
-    public function index(){
+    public function index(Request $request){
         $this->layout='admin';
         return $this->view("dashboard");
     }
