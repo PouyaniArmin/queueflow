ac66c915aa15cc7c88f12671d6828980ac78d4b5
PouyaniArmin <pouyaniarmin@gmail.com>
2026-09-08 20:03:27 +0330

    feat(service-model): refactor insert/update methods with fillable fields support

--- models/Service.php
+++ models/Service.php
@@ -3,5 +3,5 @@ namespace Models;
 
 class Service extends Models{
     protected string $table='services';
-    protected array $fillable=['business_id','name','duration_minutes','price'];
+    protected array $fillable=['business_id','name','duration_minutes','price','is_active'];
 }
